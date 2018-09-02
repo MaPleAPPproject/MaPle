bd8c9a33bfed06b0a9c4dd188fe725638825af5c
violethsu <glairmorning@gmail.com>
2018-09-02 14:54:06 +0800

    Violet: (1)完成navigation切換fragment(need debug:首頁無法顯示tab) (2)完成Explore切換頁(缺連結至個人主頁)

--- app/src/main/java/group3/MainActivity.java
+++ app/src/main/java/group3/MainActivity.java
@@ -5,34 +5,57 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
-import android.webkit.WebView;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.example.violethsu.maple.R;
 
-public class MainActivity extends Navigation_baseActivity {
+import java.util.ArrayList;
+import java.util.List;
+// need to debug mypage頁面一開始沒有tablayout
+public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private MainActivity mainActivity;
 
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+    protected BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
             switch (item.getItemId()) {
                 case R.id.navigation_mypage:
+                    getSupportActionBar().show();
+                    viewPager.setCurrentItem(0);
+//                    fragment換頁
+//                    fragment = new MypageFragment();
+//                    changeFragment(fragment);
+//                    activity換頁
+//                    Intent intent = new Intent();
+//                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+//                    intent.setClass(MainActivity.this, MypageFragment.class);
+//                    startActivity(intent);
+//                    finish();
                     return true;
-                case R.id.navigation_explore:
-                    Intent intent = new Intent();
-                    intent.setClass(MainActivity.this, ExploreActivity.class);
-                    startActivity(intent);
-                    finish();
-                case R.id.navigation_friends:
 
+                case R.id.navigation_explore:
+                    getSupportActionBar().hide();
+                    viewPager.setCurrentItem(1);
+//                    fragment = new ExploreFragment();
+//                    changeFragment(fragment);
+                    return true;
+                case R.id.navigation_friends:
+                    getSupportActionBar().show();
+                    viewPager.setCurrentItem(2);
+//                    fragment = new FriendsFragment();
+//                    changeFragment(fragment);
                     return true;
             }
             return false;
@@ -43,11 +66,82 @@ public class MainActivity extends Navigation_baseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        setupViewPager(viewPager);
+        final BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                        navigation.setSelectedItemId(R.id.navigation_mypage);
+                        break;
+                    case 1:
+                        navigation.setSelectedItemId(R.id.navigation_explore);
+                        break;
+                    case 2:
+                        navigation.setSelectedItemId(R.id.navigation_friends);
+                        break;
+//                        default:
+//                            navigation.setSelectedItemId(R.id.navigation_mypage);
+//                            initContent();
+//                            break;
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+    }
+    private void initContent() {
+        getSupportActionBar().show();
+        viewPager.setCurrentItem(0);
+//        Fragment fragment = new MypageFragment();
+//        changeFragment(fragment);
     }
 
+    private void changeFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction =
+                fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.content, fragment);
+        fragmentTransaction.commit();
+    }
+    private void setupViewPager(ViewPager viewPager) {
+        BottomNavPagerAdapter adapter = new BottomNavPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MypageFragment());
+        adapter.addFragment(new ExploreFragment());
+        adapter.addFragment(new FriendsFragment());
+        viewPager.setAdapter(adapter);
+    }
+    public class BottomNavPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mainFragmentList = new ArrayList<>();
+
+        public BottomNavPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return mainFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mainFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment) {
+            mainFragmentList.add(fragment);
+        }
+    }
 
 }
