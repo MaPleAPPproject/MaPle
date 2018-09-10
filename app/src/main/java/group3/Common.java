fd2811262ff3eb4057424d956a37ba85facb9e4c
juiyingchiu <uvoless@gmail.com>
2018-09-10 11:53:58 +0800

    add cropIntent add premium button intent add alertdialog in Mypage_User_profile add sharedPreferences in Mypage_User_profile

--- app/src/main/java/group3/Common.java
+++ app/src/main/java/group3/Common.java
@@ -1,7 +1,10 @@
 package group3;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.ConnectivityManager;
@@ -12,9 +15,12 @@ import android.support.v4.content.ContextCompat;
 import java.util.HashSet;
 import java.util.Set;
 
+import group3.mypage.Mypage_UserProfile_Activity;
+
 public class Common {
 
     public String URL = "http://10.0.2.2:8080/MaPle_Web";
+    public final static String PREF_FILE = "preference";
     public static Bitmap downSize(Bitmap srcPicture, int newSize) {
 
         if (newSize < 20) {
@@ -57,6 +63,33 @@ public class Common {
 //                (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
 //        NetworkInfo networkInfo = connctivityManager != null? connctivityManager.getActiveNetworkInfo() : null;
 //        return networkInfo != null && networkInfo.isConnected();
+//    }
+
+
+//    public void confirmExit(){
+//
+//
+//        AlertDialog.Builder  ad= new AlertDialog.Builder(Common,MainActivity.class)
+//                .setTitle("確認視窗")
+//                .setMessage("確定要離開此頁面嗎？")
+//                .setPositiveButton("確定", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//
+//
+//
+//                    }
+//                })
+//                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//
+//
+//                    }
+//                });
+//        ad.show();
+//
+//
 //    }
 
 
